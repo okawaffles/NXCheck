342c2f2df54436c427ee7251e17d51780f9bec08
Eli Clark <herbin12@herbin12.com>
2021-11-25 14:52:29 -0600

    quick commit for backup

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,90 @@
 #include <SDL.h>
+#include <SDL_mixer.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <switch.h>
+#include <unistd.h>
 
-void drawBG(SDL_Renderer *renderer) {
-    SDL_SetRenderDrawColor(renderer, 39, 40, 56);
-    SDL_Rect bg = {0, 0, 1080, 720}; //x y sx sy
-    SDL_RenderFillRect(renderer, &bg);
 
-    SDL_SetRenderDrawColor(renderer, 19, 48, 45);
-    SDL_Rect bar = {0, 0, 1080, 50};
-    SDL_RenderFillRect(renderer, &bar);
+// taken from sdl2-demo
+SDL_Texture * render_text(SDL_Renderer *renderer, const char* text, TTF_Font *font, SDL_Color color, SDL_Rect *rect) 
+{
+    SDL_Surface *surface;
+    SDL_Texture *texture;
+
+    surface = TTF_RenderText_Solid(font, text, color);
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    rect->w = surface->w;
+    rect->h = surface->h;
+
+    SDL_FreeSurface(surface);
+
+    return texture;
 }
 
+void easyDrawText(SDL_Renderer *renderer, TTF_Font *font, const char* text, SDL_Color color, int x, int y) {
+    SDL_Texture* easyDrawObj_tex = NULL;
+    SDL_Rect easyDrawObj_rect = {x, y, 0, 0};
+    easyDrawObj_tex = render_text(renderer, text, font, color, &easyDrawObj_rect);
+    SDL_RenderCopy(renderer, easyDrawObj_tex, NULL, &easyDrawObj_rect);
+    SDL_DestroyTexture(easyDrawObj_tex);
+}
+
+
+void drawBG(SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, 53, 62, 74, 255);
+    SDL_Rect bg = {0, 0, 1280, 720}; //x y sx sy
+    SDL_RenderFillRect(renderer, &bg);
+
+    SDL_SetRenderDrawColor(renderer, 55, 103, 112, 255);
+    SDL_Rect bar = {0, 0, 1280, 50};
+    SDL_RenderFillRect(renderer, &bar);
+
+
+    //nxcheck right top text
+    SDL_Texture* nxcheck_tex = NULL; 
+    SDL_Color white = {255, 255, 255};
+    TTF_Font* fontdef = TTF_OpenFont("mplus1.ttf", 36);
+    SDL_Rect nxcheck_rect = { 2, -7, 0, 0 };
+    nxcheck_tex = render_text(renderer, "NXCheck", fontdef, white, &nxcheck_rect);
+
+    //hbmenu right bottom text
+    SDL_Texture* hbmenu_tex = NULL; 
+    SDL_Rect hbmenu_rect = { 0, 720 - 45, 0, 0 };
+    hbmenu_tex = render_text(renderer, "Press + to return to hbmenu", fontdef, white, &hbmenu_rect);
+
+    SDL_RenderCopy(renderer, nxcheck_tex, NULL, &nxcheck_rect);
+    SDL_RenderCopy(renderer, hbmenu_tex, NULL, &hbmenu_rect);
+
+    // destroy textures n stuff
+    SDL_DestroyTexture(nxcheck_tex);
+    SDL_DestroyTexture(hbmenu_tex);
+    TTF_CloseFont(fontdef);
+}
+
+
+void drawOptions(SDL_Renderer *renderer) {
+    SDL_Color white = {255, 255, 255};
+    TTF_Font* fontdef = TTF_OpenFont("mplus1.ttf", 24);
+
+    SDL_SetRenderDrawColor(renderer, 55, 103, 112, 255);
+    SDL_Rect op0 = {0, 75, 250, 50};
+    SDL_RenderFillRect(renderer, &op0);
+    easyDrawText(renderer, fontdef, "Touchscreen", white, 5, 80);
+
+
+
+    //clean up
+    TTF_CloseFont(fontdef);
+}
+
+
+
+
 int main(int argc, char *argv[]) {
+    romfsInit();
+    chdir("romfs:/");
+
     SDL_Event event;
     SDL_Window *window;
     SDL_Renderer *renderer;
@@ -20,6 +94,8 @@ int main(int argc, char *argv[]) {
         SDL_Log("SDL_Init: %s\n", SDL_GetError());
         return -1;
     }
+
+    TTF_Init();
 
     window = SDL_CreateWindow("sdl2_gles2", 0, 0, 1920, 1080, 0); //create a handheld window, remember to add on docked support.
     if (!window) {
@@ -82,7 +158,8 @@ int main(int argc, char *argv[]) {
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        drawBG(renderer);    
+        drawBG(renderer);
+        drawOptions(renderer);
 
         SDL_RenderPresent(renderer);
     }
